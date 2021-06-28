2d7a04a74375aa9b921482cd200601c0c94a0f49
Alberto Cabrera <alberto.gramer@gmail.com>
2021-06-28 17:43:11 +0200

    Music and SFX Volume Controllers added

---
+++ Scripts/SFXSliderVolume.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+namespace Alprogram.Alvalues
+{
+    public class SFXSliderVolume : MonoBehaviour
+    {
+        /// <summary>
+        /// The name of the exposed paramerter of the Audio mixer
+        /// </summary>
+        [SerializeField] private string group;
+        [SerializeField] private AudioMixer master;
+        [SerializeField] Slider slider;
+
+        private void Awake()
+        {
+            slider.onValueChanged.AddListener(SliderVolumeController);
+        }
+
+        private void OnDisable()
+        {
+            slider.onValueChanged.RemoveListener(SliderVolumeController);
+        }
+
+        private void SliderVolumeController(float value)
+        {
+            master.SetFloat(group, value);
+        }
+    }
+}

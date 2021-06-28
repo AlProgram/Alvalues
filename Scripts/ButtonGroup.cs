b2e8c17a0868b7933241ff35f044d4fa2dc07065
Alberto Cabrera <alberto.gramer@gmail.com>
2021-06-28 18:07:40 +0200

    Button Group update

--- Scripts/ButtonGroup.cs
+++ Scripts/ButtonGroup.cs
@@ -2,38 +2,93 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using TMPro;
 using UnityEngine.UI;
+using UnityEngine.Events;
 
 namespace Alprogram.Alvalues
 {
-
-    [RequireComponent(typeof(Button))]
-    public class ButtonGroup : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+    /// <summary>
+    /// Need a component that can be raycasted
+    /// </summary>
+    public class ButtonGroup : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
     {
+        [SerializeField] private UnityEvent buttonEvent;
+
         [Header("Colors")]
         [SerializeField] private Color NormalColor;
         [SerializeField] private Color HighlightedColor;
+        [SerializeField] private Color PressedColor;
+        [SerializeField] private Color DisabledColor;
 
         [Header("Components")]
         [SerializeField] private TextMeshProUGUI[] texts;
         [SerializeField] private Image[] imgs;
 
+        private bool disabled = false;
+        public bool Disable
+        {
+            get => disabled;
+            set
+            {
+                disabled = value;
+                ChangeDisable();
+            }
+        }
+
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            if (!disabled)
+            {
+                foreach (var item in texts)
+                    item.color = PressedColor;
+
+                foreach (var item in imgs)
+                    item.color = PressedColor;
+
+                buttonEvent?.Invoke();
+            }
+        }
 
         public void OnPointerEnter(PointerEventData eventData)
         {
-            foreach (var item in texts)
-                item.color = HighlightedColor;
+            if (!disabled)
+            {
+                foreach (var item in texts)
+                    item.color = HighlightedColor;
 
-            foreach (var item in imgs)
-                item.color = HighlightedColor;
+                foreach (var item in imgs)
+                    item.color = HighlightedColor;
+            }
         }
 
         public void OnPointerExit(PointerEventData eventData)
         {
-            foreach (var item in texts)
-                item.color = NormalColor;
+            if (!disabled)
+            {
+                foreach (var item in texts)
+                    item.color = NormalColor;
 
-            foreach (var item in imgs)
-                item.color = NormalColor;
+                foreach (var item in imgs)
+                    item.color = NormalColor;
+            }
+        }
+
+        private void ChangeDisable()
+        {
+            if (!disabled)
+            {
+                foreach (var item in texts)
+                    item.color = NormalColor;
+                foreach (var item in imgs)
+                    item.color = NormalColor;
+            }
+
+            else
+            {
+                foreach (var item in texts)
+                    item.color = DisabledColor;
+                foreach (var item in imgs)
+                    item.color = DisabledColor;
+            }
         }
     }
 }
